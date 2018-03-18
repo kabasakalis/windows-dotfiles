9faeb863dc5b971c1d72ea783681bcf0d44add2f
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-03-18 02:44:10 +0200

    Added _vsvimrc

--- dotfiles.bat
+++ dotfiles.bat
@@ -5,6 +5,7 @@ SET VIM_HOME="C:\Program Files (x86)\Vim"
 SET VIM_RUNTIME="C:\Program Files (x86)\Vim\vim80"
 
 mklink  %userprofile%\_vimrc %DOTFILES_HOME%\vim\_vimrc
+mklink  %userprofile%\_vsvimrc %DOTFILES_HOME%\vim\_vsvimrc
 mklink  %userprofile%\plugins.vim  %DOTFILES_HOME%\vim\plugins.vim
 
 mklink  %VIM_RUNTIME%\autoload\plug.vim  %DOTFILES_HOME%\vim\autoload\plug.vim
