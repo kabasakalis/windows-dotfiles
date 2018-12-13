a6e0c730b6190dee3f01e06d72740755d3a4d3af
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-12-13 02:42:08 +0200

    spacevim cpp workflow updates

--- dotfiles.bat
+++ dotfiles.bat
@@ -22,7 +22,6 @@ mklink  %VIM_RUNTIME%\autoload\plug.vim  %WIN_DOTFILES_HOME%\vim\autoload\plug.v
 mklink  %VIM_RUNTIME%\autoload\utils.vim  %WIN_DOTFILES_HOME%\vim\autoload\utils.vim
 mklink  %VIM_RUNTIME%\colors\Tomorrow-Night.vim  %WIN_DOTFILES_HOME%\vim\color\Tomorrow-Night.vim
 
-
 rem SPACEVIM dotfiles
 mklink /J %userprofile%\.SpaceVim.d %UBUNTU_DOTFILES_HOME%\spacevim
 
@@ -31,13 +30,8 @@ mklink  %userprofile%\.ideavimrc %UBUNTU_DOTFILES_HOME%\neovim\.ideavimrc
 
 rem ccls.exe Language Server Protocol for Spacevim C++ IDE features 
 mklink  %MSYS_ROOT%\bin\ccls.exe %DROPBOX%\dev\cpp\ccls.exe
-
-rem compile_commands_fix.sh
-mklink  %MSYS_ROOT%\bin\compile_commands_fix.sh %UBUNTU_DOTFILES_HOME%\spacevim\bin\compile_commands_fix.sh
-
 rem  useful executables
 mklink /J F:\tools   %WIN_DOTFILES_HOME%\tools
-
 
 rem Attempt for NeoVim  dotfiles, NeoVim sucks on Windows.
 rem mklink  %NEOVIM_RUNTIME%\init.vim  %DOTFILES_HOME%\neovim\init.vim
@@ -46,6 +40,5 @@ rem mklink /J  %NEOVIM_RUNTIME%\autoload  %DOTFILES_HOME%\neovim\autoload
 rem mklink /J  %NEOVIM_RUNTIME%\colors  %DOTFILES_HOME%\neovim\colors
 rem mklink /J  %NEOVIM_RUNTIME%\colors  %DOTFILES_HOME%\neovim\colors
 rem mklink /J  %VIM_RUNTIME%\colors  %DOTFILES_HOME%\neovim\colors
-
 
 timeout 100
