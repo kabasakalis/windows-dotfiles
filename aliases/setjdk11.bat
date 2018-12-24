846cd9d180beccd383d42cd33545c2d5b3199693
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-12-24 14:38:14 +0200

    Move bat files to aliases

---
+++ aliases/setjdk11.bat
@@ -0,0 +1,6 @@
+@echo off
+set "JAVA_HOME=F:\jdk-11"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+echo - Java is now version 11.
+echo.
+@echo on
---
+++ aliases/setjdk8.bat
@@ -0,0 +1,6 @@
+@echo off
+set "JAVA_HOME=F:\jdk-8"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+echo - Java is now version 8.
+echo.
+@echo on
